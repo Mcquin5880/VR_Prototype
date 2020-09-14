fcc84357bc080535944a66a81e613cfa3c5df4ef
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-13 23:37:00 -0400

    FIXING AI OMG

---
+++ VR_Prototype/Assets/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    [SerializeField] float timeUntilDestroyed = 5f;
+
+    private void Start()
+    {
+        StartCoroutine(DestroyAfterDelayRoutine());
+    }
+
+    private IEnumerator DestroyAfterDelayRoutine()
+    {
+        yield return new WaitForSeconds(timeUntilDestroyed);
+        Destroy(gameObject);
+    }
+
+}
---
+++ VR_Prototype/Assets/Scripts/EnemyShootTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShootTest : MonoBehaviour
+{
+    [SerializeField] GameObject laserPrefab;
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float projectileSpeed = 50f;
+
+    // testing
+    bool canShoot = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FireRepeatedly());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator FireRepeatedly()
+    {
+        while (canShoot)
+        {
+            GameObject laser = Instantiate(laserPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+            laser.GetComponent<Rigidbody>().velocity = spawnPoint.transform.forward * projectileSpeed;
+            canShoot = false;
+            yield return new WaitForSeconds(2f);
+            canShoot = true;
+        }       
+    }
+}
