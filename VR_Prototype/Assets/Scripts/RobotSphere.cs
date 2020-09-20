f30b1023734eec2018c271a71c19ef0dad5e3efa
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-20 02:21:32 -0400

    Forged3d asset import and sphere robot

---
+++ VR_Prototype/Assets/Scripts/RobotSphere.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSphere : MonoBehaviour
+{
+
+    AudioSource audioSource;
+    [SerializeField] AudioClip sphereOpenAudio, chargingUpAudio, dashAudio;
+    Animator anim;
+    RobotSphere_Child_AnimDriver robotSphereChild;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+        robotSphereChild = GetComponentInChildren<RobotSphere_Child_AnimDriver>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayImpactAudio()
+    {
+        audioSource.Play();
+    }
+
+    public void PlayOpenSphereAudio()
+    {
+        audioSource.PlayOneShot(sphereOpenAudio);
+    }
+
+    public void PlayChargingUpAudio()
+    {
+        audioSource.PlayOneShot(chargingUpAudio);
+    }
+
+    public void PlayDashAudio()
+    {
+        audioSource.PlayOneShot(dashAudio);
+    }
+
+    // -----------------------------------------------------------------------------------------------
+
+    public void PlayRobotSphereOpenAnimation()
+    {
+        robotSphereChild.SetOpenAnimation();
+    }
+
+    public void PlayCloseIntoRollAnimation()
+    {
+        robotSphereChild.SetCloseIntoRollAnimation();
+    }
+
+    public void PlayImpactAnimation()
+    {
+        robotSphereChild.SetImpactStopSpinAnimation();
+    }
+}
---
+++ VR_Prototype/Assets/Scripts/RobotSphere_Child_AnimDriver.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobotSphere_Child_AnimDriver : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetOpenAnimation()
+    {
+        GetComponent<Animator>().SetTrigger("sphere_Open");
+    }
+
+    public void SetCloseIntoRollAnimation()
+    {
+        GetComponent<Animator>().SetTrigger("sphere_Close_Into_Roll");
+    }
+
+    public void SetImpactStopSpinAnimation()
+    {
+        GetComponent<Animator>().SetTrigger("impact_Stop_Spin");
+    }
+}
