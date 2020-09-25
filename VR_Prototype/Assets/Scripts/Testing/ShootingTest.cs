55a7b64c34ba1f9c72163fe4a2249f451e8b939f
NewbieMcCoder <Mquinter1988@gmail.com>
2020-09-24 23:09:39 -0400

    Testinge enemy and reimport

---
+++ VR_Prototype/Assets/Scripts/Testing/ShootingTest.cs
@@ -0,0 +1,50 @@
+﻿using DarkTonic.MasterAudio;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingTest : MonoBehaviour
+{
+    [SerializeField] Transform spawnPoint;
+    [SerializeField] float projectileSpeed = 100f;
+    [SerializeField] float timeBetweenShots = .25f;
+    [SerializeField] GameObject playerLaserPrefab;
+
+    Coroutine firingCoroutine;
+    bool canShoot = true;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            firingCoroutine = StartCoroutine(FirePlayerLaser());
+        }
+        if (Input.GetButtonUp("Fire1"))
+        {
+            StopAllCoroutines();
+        }
+        
+    }
+
+    IEnumerator FirePlayerLaser()
+    {
+        while (true)
+        {
+            Debug.Log("Hey there");
+
+            GameObject laser = Instantiate(playerLaserPrefab, spawnPoint.position, spawnPoint.rotation) as GameObject;
+            laser.GetComponent<Rigidbody>().velocity = spawnPoint.transform.forward * projectileSpeed;
+            MasterAudio.PlaySound3DAtTransform("PlayerLaser", spawnPoint);
+
+            yield return new WaitForSeconds(timeBetweenShots);
+        }
+    }
+}
